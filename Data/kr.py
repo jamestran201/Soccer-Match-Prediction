91190c5435064a80772f4ea8d6b7bb3838ad5a86
Nicholas Hallman <ngahallman@gmail.com>
2018-11-22 23:11:57 -0500

    Update kr.py

--- Data/kr.py
+++ Data/kr.py
@@ -16,37 +16,53 @@ games = []
 previousSet = {}
 inf.readline()
 
+def whichWin(d):
+    if d[0][3] > d[1][3]:
+        return 1
+    return 0
+
 for line in inf:
     data = line.strip().split(",")
     if data[7] != "NA":
         side = int(data[7]) - 1
         currentSet = {data[8], data[9]}
+        size = len(games) - 1
         if not currentSet == previousSet:
-            games.append([[0 for i in range(9 + 15)],[0 for i in range(9 + 15)]])
+            if len(games) != 0:
+                games[size].append(whichWin(games[size]))
+            games.append([[0 for i in range(7 + 15 + 4 + 13)],[0 for i in range(7 + 15 + 4 + 13)]])
             previousSet = currentSet
         data = clense(data)
         #print(data)
         size = len(games) - 1
         games[size][side][0] += 1
-        games[size][side][1] += (int(data[14]))
-        games[size][side][2] += (int(data[15]))
-        games[size][side][3] += (int(data[16]))
-        games[size][side][4] += (int(data[17]))
-        games[size][side][5] += (int(data[18]))
-        games[size][side][6] += (int(data[19]))
-        games[size][side][7] += (int(data[20]))
-        games[size][side][8] += (int(data[21]))
-        games[size][side][int(data[5]) + 8] += 1
+        if int(data[15]) != 0:
+            games[size][side][1] += 1
+        games[size][side][2] += (int(data[16]))
+        if int(data[19]) != 0:
+            games[size][side][3] += 1
+        games[size][side][5] += (int(data[21]))
+        games[size][side][int(data[5])  + 6] += 1
+        games[size][side][int(data[20]) + 6 + 15] += 1
+        games[size][side][int(data[14]) + 6 + 15 + 4] += 1
 
-print("events, shot place, shot_outcome, is goal, location, body part, assist meathod, situation, fast break", file = outf, end = ",")
-print("e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11,e12,e13,e14,e15", file=outf, end = ",") 
-print("event type 2, shot place 2, shot outcome 2, is goal 2, location 2, body part 2, assist meathod 2, situation 2, fast break 2", file=outf, end = ",")
-print("e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11,e12,e13,e14,e15", file=outf) 
+print("events, shots made, is goal, assist count, fast break", file = outf, end = ",")
+print("e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11,e12,e13,e14,e15", file=outf, end = ",")
+print("s1,s2,s3,s4", file=outf, end = ",")
+print("sp1,sp2,sp3,sp4,sp5,sp6,sp7,sp8,sp9,sp10,sp11,sp12,sp13", file=outf, end = ",")
+print("events 2, shots made 2, is goal 2, assist count 2, fast break 2", file=outf, end = ",")
+print("e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11,e12,e13,e14,e15,TARGET", file=outf, end=",") 
+print("s1,s2,s3,s4", file=outf, end = ",")
+print("sp1,sp2,sp3,sp4,sp5,sp6,sp7,sp8,sp9,sp10,sp11,sp12,sp13", file=outf)
+
 
 for game in games:
     for t in game:
-        for elem in t:
-            print("{}".format(elem), file=outf, end=",")
+        try:
+            for elem in t:
+                print("{}".format(elem), file=outf, end=",")
+        except:
+            print("{}".format(t), file=outf, end=",")
     print("",file=outf)
 
 inf.close()
